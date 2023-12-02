313352406284443d51706a9d4fda4c187338d588
Zuong <quocvuongle.ct@gmail.com>
2023-12-02 15:50:26 +0700

    add new layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Navigation from "./components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { readonly children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <Navbar />
+            <body className={inter.className}>
+                {children}
+                <Navigation />
+            </body>
         </html>
     );
 }
